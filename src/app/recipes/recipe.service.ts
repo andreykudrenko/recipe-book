cfdeb235dc942d0fba377a58fe22ebd6ac2b1fff
AndreyKudrenko <a.v.kudrenko@gmail.com>
2019-08-03 11:03:49 +0300

    storing data was added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -8,25 +8,27 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChages = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Beef Steak',
-      'Excellent and very healthy beef steak',
-      'https://static.1000.menu/img/content/25349/pravilnyi-steik-iz-govyadiny_1518083846_13_max.jpg',
-      [
-        new Ingredient('Beef Meat', 1),
-        new Ingredient('Tomatoes', 2),
-      ]),
-    new Recipe(
-      'Big Fat Burger',
-      'What else you need to say?',
-      'http://primebeef.ru/images/cms/data/img_3911.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Cheese', 1),
-        new Ingredient('Meat', 1),
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Beef Steak',
+  //     'Excellent and very healthy beef steak',
+  //     'https://static.1000.menu/img/content/25349/pravilnyi-steik-iz-govyadiny_1518083846_13_max.jpg',
+  //     [
+  //       new Ingredient('Beef Meat', 1),
+  //       new Ingredient('Tomatoes', 2),
+  //     ]),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'What else you need to say?',
+  //     'http://primebeef.ru/images/cms/data/img_3911.jpg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Cheese', 1),
+  //       new Ingredient('Meat', 1),
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
   constructor(private slService: ShoppingListService) {}
 
   getRecipes(): Recipe[] {
@@ -35,6 +37,11 @@ export class RecipeService {
 
   getRecipe(id: number): Recipe {
     return this.recipes[id];
+  }
+
+  setRecipe(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChages.next(this.recipes.slice());
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
